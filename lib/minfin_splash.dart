8369bfad7c497e258b568193a47cd2b614a08a77
abbos2101 <abbos2101@gmail.com>
2022-11-22 10:30:17 +0500

    optimize code

--- lib/minfin_splash.dart
+++ lib/minfin_splash.dart
@@ -1,4 +1,3 @@
 library minfin_splash;
 
 export 'src/minfin_splash.dart';
-export 'src/minfin_splash_model.dart';
--- lib/src/minfin_splash.dart
+++ lib/src/minfin_splash.dart
@@ -1,23 +1,13 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'minfin_splash_model.dart';
 
 class MinfinSplash extends StatefulWidget {
-  final Future<MinfinSplashModel> Function(BuildContext context) setup;
-  final Function(BuildContext context, MinfinSplashModel model) onNext;
+  final Future<void> Function(BuildContext context) setup;
   final WidgetBuilder builder;
-  final Function()? dispose;
-  final bool debugWaiting;
-  final Duration duration;
 
   const MinfinSplash({
     Key? key,
     required this.setup,
-    required this.onNext,
     required this.builder,
-    this.dispose,
-    this.debugWaiting = false,
-    this.duration = const Duration(seconds: 2),
   }) : super(key: key);
 
   @override
@@ -27,26 +17,8 @@ class MinfinSplash extends StatefulWidget {
 class _MinfinSplashState extends State<MinfinSplash> {
   @override
   void initState() {
-    load();
+    widget.setup(context);
     super.initState();
-  }
-
-  Future<void> load() async {
-    final start = DateTime.now();
-    final model = await widget.setup(context);
-    if (widget.debugWaiting || kReleaseMode) {
-      final difference = DateTime.now().difference(start);
-      if (difference < widget.duration) {
-        await Future.delayed(widget.duration - difference);
-      }
-    }
-    if (mounted) widget.onNext(context, model);
-  }
-
-  @override
-  void dispose() {
-    widget.dispose?.call();
-    super.dispose();
   }
 
   @override
